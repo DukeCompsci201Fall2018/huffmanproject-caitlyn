7d797e92b3e6c105a44fdcb83507ac32bb498514
Caitlyn Carlisle <caitlyn.carlisle@duke.edu>
2018-12-07 15:21:11 -0500

    new

--- src/HuffMainDecompress.java
+++ src/HuffMainDecompress.java
@@ -2,6 +2,10 @@ import java.io.File;
 import java.io.*;
 
 public class HuffMainDecompress {
+	private static final int PSUEDO_EOF = 0;
+	private static final int BITS_PER_INT = 0;
+	private static final int BITS_PER_WORD = 0;
+
 	public static void main(String[] args) {
 		
 		System.out.println("Huffman Decompress Main");
@@ -26,4 +30,72 @@ public class HuffMainDecompress {
 		long diff2 = bos.bitsWritten() - bis.bitsRead();
 		System.out.printf("%d compared to %d\n",diff,diff2);
 	}
-}
+	
+	
+	
+	public void decompress(BitInputStream in, BitOutputStream out) {
+		
+		int bits = in.readBits(BITS_PER_INT);
+		if(bits !=32) {
+			throw new HuffException("illegal header starts with "+bits);
+			
+		}
+		HuffNode root = readTreeHeader(in);
+		readCompressedBits(root,in,out);
+		out.close();
+	}
+	
+	public HuffNode readTreeHeader(BitInputStream in) {
+
+
+		int bit = in.readBits(BITS_PER_WORD);
+		
+		HuffNode root = new HuffNode(BITS_PER_WORD+1,0);
+		
+		
+		if (bit == -1) throw new HuffException("illegal bit");
+		
+		if (bit == 0) {
+			root.myLeft = readTreeHeader(in);
+			root.myRight = readTreeHeader(in);
+					return new HuffNode(0,0,root.myLeft,root.myRight);
+		}
+		else {
+			int value = in.readBits(9);
+					return new HuffNode(value,0,null,null);
+		
+		}
+	}
+	
+		public void readCompressedBits(HuffNode root, BitInputStream in, BitOutputStream out) {
+
+			//HuffNode end = new HuffNode(PSUEDO_EOF,0);
+			
+			
+			HuffNode current = root; 
+			while (true) {
+				int bits = in.readBits(1);
+				if (bits == -1) {
+					throw new HuffException("bad input, no PSEUDO_EOF");
+				}
+				else { 
+					if (bits == 0) current = current.myLeft;
+					else current = current.myRight;
+
+					if (bits == 1) {
+						if (current.myValue == PSUEDO_EOF) 
+							break;   // out of loop
+						else {
+							current.myValue = in.readBits(9);
+									current = root; // start back after leaf
+						}
+					}
+				}
+			}
+
+
+
+		}
+
+
+		}
